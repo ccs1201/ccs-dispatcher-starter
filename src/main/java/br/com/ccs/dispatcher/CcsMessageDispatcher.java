983a45a1d4fd39e086b81f0d93cf9b6e87cd2d43
Cleber.Ssouza <ccs1201@gmail.com>
2025-05-09 21:04:46 -0300

    doc: Adicionando documentação e licença.

--- src/main/java/br/com/ccs/dispatcher/CcsMessageDispatcher.java
+++ src/main/java/br/com/ccs/dispatcher/CcsMessageDispatcher.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Cleber Souza
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 package br.com.ccs.dispatcher;
 
@@ -15,6 +31,15 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 
 import java.util.logging.Logger;
 
+/**
+ * Classe responsável por receber as mensagens do RabbitMQ e despachá-las para o handler correto.
+ * <p>
+ * Class responsible for receiving RabbitMQ messages and dispatching them to the correct handler.
+ *
+ * @author Cleber Souza
+ * @version 1.0
+ * @since 09/05/2025
+ */
 public class CcsMessageDispatcher {
 
     private static final Logger log = Logger.getLogger(CcsMessageDispatcher.class.getName());
@@ -37,10 +62,10 @@ public class CcsMessageDispatcher {
             concurrency = "#{@ccsDispatcherProperties.concurrency}",
             returnExceptions = "true")
     public Object onMessage(Message message) {
-        log.info("Mensagem recebida: " + message);
 
         try {
             MessageWrapper messageWrapper = objectMapper.readValue(message.getBody(), MessageWrapper.class);
+            log.info("Mensagem recebida: " + messageWrapper);
 
             // Cria request com método e path
             MockHttpServletRequest request = new MockHttpServletRequest(
