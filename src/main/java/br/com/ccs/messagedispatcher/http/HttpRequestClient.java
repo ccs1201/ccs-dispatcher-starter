dd53f34dc40365ff0a2eacbb085e74c57f0a3fca
Cleber.Ssouza <ccs1201@gmail.com>
2025-05-26 22:46:35 -0300

    #HttpRequestClient Removido

--- src/main/java/br/com/ccs/messagedispatcher/http/HttpRequestClient.java
+++
@@ -1,8 +0,0 @@
-package br.com.ccs.messagedispatcher.http;
-
-import br.com.ccs.messagedispatcher.messaging.model.MessageWrapper;
-
-public interface HttpRequestClient {
-
-    Object doRequest(MessageWrapper messageWrapper);
-}
