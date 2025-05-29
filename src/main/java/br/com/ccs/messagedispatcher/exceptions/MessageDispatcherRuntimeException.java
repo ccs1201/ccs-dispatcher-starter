9327679e86a6f949887aca98fe2609609d0fd56b
Cleber Souza <cleber.constante@totvs.com.br>
2025-05-29 10:30:26 -0300

    #refact: Removendo o atributo appName do MessageDispatcherRuntimeException

--- src/main/java/br/com/ccs/messagedispatcher/exceptions/MessageDispatcherRuntimeException.java
+++ src/main/java/br/com/ccs/messagedispatcher/exceptions/MessageDispatcherRuntimeException.java
@@ -1,16 +1,12 @@
 package br.com.ccs.messagedispatcher.exceptions;
 
-import br.com.ccs.messagedispatcher.util.EnvironmentUtils;
-
 public class MessageDispatcherRuntimeException extends RuntimeException {
 
-    private static final String appName = EnvironmentUtils.getAppName();
-
     public MessageDispatcherRuntimeException(String message, Throwable cause) {
-        super(appName + ": " + message, cause);
+        super(message, cause);
     }
 
     public MessageDispatcherRuntimeException(String message) {
-        super(appName + ": " + message);
+        super(message);
     }
 }
