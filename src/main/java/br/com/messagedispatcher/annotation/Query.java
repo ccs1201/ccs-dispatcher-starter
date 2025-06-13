200b64cc7cb89116f9961b7c8a32e2add8239758
Cleber.Ssouza <ccs1201@gmail.com>
2025-06-13 20:29:19 -0300

    chore: Melhorias na documentação

--- src/main/java/br/com/messagedispatcher/annotation/Query.java
+++ src/main/java/br/com/messagedispatcher/annotation/Query.java
@@ -9,10 +9,24 @@ import java.lang.annotation.Target;
 import static br.com.messagedispatcher.constants.MessageDispatcherConstants.HandlerType;
 
 /**
- * Anotação para marcar métodos que processam mensagens do tipo Query.
+ * <h3>
+ * Anotação para marcar métodos que processam mensagens do tipo Query.</br>
  * Annotation to mark methods that handle Query messages.
+ * </h3>
+ * <p>
+ * Queries são executados e devem retornar um resultado.
+ * <p>
+ * Queries devem ser idempotentes, ou seja, podem ser executados mais de uma vez com o mesmo resultado.
+ * <p>
+ * Apenas recuperam dados do sistema (read operations)
+ * <p>
+ * Não causam mudanças de estado
+ * <p>
+ * São otimizadas para leitura
+ * <p>
+ * Exemplo: GetProducts, FindOrderById, ListCustomers
  */
-@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @MessageHandler(handlerType = HandlerType.QUERY)
